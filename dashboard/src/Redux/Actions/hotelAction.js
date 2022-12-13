42ae38ef5edc6edd3d2832726e23cf47543faa19
zubair <mohammadzubairkhan007@gmail.com>
2022-12-13 14:35:10 +0500

    Update hotelAction.js

--- dashboard/src/Redux/Actions/hotelAction.js
+++ dashboard/src/Redux/Actions/hotelAction.js
@@ -1,14 +1,11 @@
 import {
-    GET_ALL_VACATION_PRODUCTS_REQUEST,
-    GET_ALL_VACATION_PRODUCTS_SUCCESS,
-    GET_ALL_VACATION_PRODUCTS_FAIL,
-    GET_VACATION_PRODUCTS_BY_SLUG_REQUEST,
-    GET_VACATION_PRODUCTS_BY_SLUG_SUCCESS,
-    GET_VACATION_PRODUCTS_BY_SLUG_FAIL,
-    NEW_HOTEL_REQUEST,
-    NEW_HOTEL_SUCCESS,
-    NEW_HOTEL_FAIL,
-    NEW_HOTEL_RESET,
+    GET_HOTEL_BY_SLUG_REQUEST,
+    GET_HOTEL_BY_SLUG_SUCCESS,
+    GET_HOTEL_BY_SLUG_FAIL,
+    CREATE_NEW_HOTEL_REQUEST,
+    CREATE_NEW_HOTEL_SUCCESS,
+    CREATE_NEW_HOTEL_FAIL,
+    CREATE_NEW_HOTEL_RESET,
     GET_ALL_HOTELS_REQUEST,
     GET_ALL_HOTELS_SUCCESS,
     GET_ALL_HOTELS_FAIL,
@@ -17,6 +14,33 @@ import {
     DELETE_HOTEL_FAIL,
   } from "../Constants/hotelConstants";
   import axios from "../helpers/axios";
+
+// Create New Hotel
+export const createHotel = (form) => {
+  return async dispatch => {
+    dispatch({ type:  CREATE_NEW_HOTEL_REQUEST });
+    try {
+      const res = await axios.post("/vacation/Hotel/add", form);
+      console.log("res......",res)
+      if (res.status === 201) {
+        dispatch({
+          type: CREATE_NEW_HOTEL_SUCCESS,
+          payload: res.data.hotel,
+        });
+      } else {
+        dispatch({
+          type: CREATE_NEW_HOTEL_FAIL,
+          payload: res.data.error,
+        });
+      }
+    } catch (error) {
+      console.log(error.message);
+    //   if(e instance of BSONTypeError){
+    //  }
+    }
+
+  };
+};
 
 // Get All Hotels For Admin
 export const getAllHotels = () => async (dispatch) => {
@@ -36,6 +60,25 @@ export const getAllHotels = () => async (dispatch) => {
     });
   }
 };
+
+//get Product by Slug
+export const getHotelBySlug = (slug) => async (dispatch) => {
+  try {
+    dispatch({ type: GET_HOTEL_BY_SLUG_REQUEST });
+    const { data } = await axios.get(`/hotels/${slug}`);
+    console.log(data);
+    dispatch({
+      type: GET_HOTEL_BY_SLUG_SUCCESS,
+      payload: data.hotel,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_HOTEL_BY_SLUG_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 
  // Delete Product
  export const deleteHotel = (id) => async (dispatch) => {
