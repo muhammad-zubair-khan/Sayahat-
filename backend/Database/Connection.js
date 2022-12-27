dae8b729b14005e723f9b9d407d0ad7a728a9d2d
zubair <mohammadzubairkhan007@gmail.com>
2022-12-27 18:23:41 +0500

    Update Connection.js

--- backend/Database/Connection.js
+++ backend/Database/Connection.js
@@ -3,7 +3,7 @@ require('../.env')
 const env = require("dotenv");
 env.config();
 // mongodb+srv://zubair:<PPzKNLjGxUL2kodH>@cluster0.qcg9aiu.mongodb.net/test
-var mongoDBURL = 'mongodb+srv://zubairkhan:PPzKNLjGxUL2kodH@cluster0.kop9x3h.mongodb.net/test'
+var mongoDBURL = process.env.MONGODBURL || 'mongodb+srv://zubairkhan:PPzKNLjGxUL2kodH@cluster0.kop9x3h.mongodb.net/test'
 mongoose.connect(mongoDBURL , {useUnifiedtopology :true , useNewUrlParser : true}).then((data)=>{
     console.log(`mongodb connected with server successfully: ${data.connection.port} `);
 })
